22478b23df95e474db1caf1f9c743b9dca4399c0
chrisw <chrisw@soe.ucsc.edu>
2018-02-22 13:25:06 -0800

    update help files

--- lib/help.sh
+++ lib/help.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cat help.txt
+echo "This is a docker image for converting cluster data files or gmt files to protograph files."
 echo ""
-echo "### example_clusters_metadata.txt ###"
+echo "for help with sample clustering:"
+echo "docker run --entrypoint ./help_sample_cluster.sh stuartlab/convert_cluster_data_to_protograph"
 echo ""
-cat /example/example_clusters_metadata.txt
+echo "for help with gmt files:"
+echo "docker run --entrypoint ./help_gmt.sh stuartlab/convert_cluster_data_to_protograph"
 echo ""
-echo "### example_clusters_for_bmeg.txt ###"
-echo ""
-head -n 10 /example/example_clusters_for_bmeg.txt
