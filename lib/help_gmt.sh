9514120d5aa3d909230475cbec9d846e2f772c26
chrisw <chrisw@soe.ucsc.edu>
2018-02-22 13:24:38 -0800

    include GMT scripts in docker image and update help files

---
+++ lib/help_gmt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat help_gmt.txt
+echo ""
+echo "### example_genesets_metadata.txt ###"
+echo ""
+cat /example/example_genesets_metadata.txt
+echo ""
+echo "### example_genesets.gmt ###"
+echo ""
+head -n 10 /example/example_genesets.gmt
---
+++ lib/help_sample_cluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat help_sample_cluster.txt
+echo ""
+echo "### example_clusters_metadata.txt ###"
+echo ""
+cat /example/example_clusters_metadata.txt
+echo ""
+echo "### example_clusters_for_bmeg.txt ###"
+echo ""
+head -n 10 /example/example_clusters_for_bmeg.txt
